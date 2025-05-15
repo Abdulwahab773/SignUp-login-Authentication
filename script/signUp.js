a643849a57720678eb57e124b9588da291007fad
Abdulwahab773 <abdulwahabyou@gmail.com>
2025-05-16 02:51:02 +0500

    resolved google signup issue

--- script/signUp.js
+++ script/signUp.js
@@ -13,8 +13,6 @@ const signUP = () => {
         createUserWithEmailAndPassword(auth, signUpEmail.value, signUpPass.value)
             .then((userCredential) => {
                 const user = userCredential.user;
-                // console.log("user-->", user);
-                
                 if (user) {
                     Swal.fire({
                         icon: "success",
@@ -29,8 +27,6 @@ const signUP = () => {
                 });
             })
             .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
                 Swal.fire({
                     icon: "error",
                     title: "Oops...",
@@ -60,13 +56,7 @@ const signUpWithGoogle = () => {
             const credential = GoogleAuthProvider.credentialFromResult(result);
             const token = credential.accessToken;
             const user = result.user;
-            // console.log(user);
-
         }).catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            const email = error.customData.email;
-            const credential = GoogleAuthProvider.credentialFromError(error);
             console.log(error);
         });
 }
